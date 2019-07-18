ebc4e67eee6fdea803378c7edc677bc300cb35e9
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-07-18 18:23:10 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="corpo" style="background-color: #ccc; text-align: center;">
+		<h1>Página principal do site da loja filial</h1>
+	</div>
+</body>
+</html>
