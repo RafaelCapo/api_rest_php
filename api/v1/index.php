f707f01063b587e7f76c165d304c4581c6853a5f
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-07-18 18:23:56 -0300

    Add files via upload

---
+++ api/v1/index.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+require_once 'classes/Estoque.php';
+
+class Rest
+{
+	public static function open($requisicao)
+	{
+		$url = explode('/', $requisicao['url']);
+		
+		$classe = ucfirst($url[0]);
+		array_shift($url);
+
+		$metodo = $url[0];
+		array_shift($url);
+
+		$parametros = array();
+		$parametros = $url;
+
+		try {
+			if (class_exists($classe)) {
+				if (method_exists($classe, $metodo)) {
+					$retorno = call_user_func_array(array(new $classe, $metodo), $parametros);
+
+					return json_encode(array('status' => 'sucesso', 'dados' => $retorno));
+				} else {
+					return json_encode(array('status' => 'erro', 'dados' => 'Método inexistente!'));
+				}
+			} else {
+				return json_encode(array('status' => 'erro', 'dados' => 'Classe inexistente!'));
+			}	
+		} catch (Exception $e) {
+			return json_encode(array('status' => 'erro', 'dados' => $e->getMessage()));
+		}
+		
+	}
+}
+
+if (isset($_REQUEST)) {
+	echo Rest::open($_REQUEST);
+}
